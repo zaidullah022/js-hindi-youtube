6c6555f82bc12e7ad60723c5ac7de9a516953c37
zaidullah022 <zaidullah022@gmail.com>
2026-04-28 07:19:31 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId =144553
+let accountEmail ="zaidullah2424@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+ 
+//accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword ="212121"
+accountCity = "Benglore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity , accountState])
+
+
+/*
+
+Prefer not to use var
+because of issue in block sope and functional scope
+*/
---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat  all JS code as newer version
+
+//alert(3+3)// we are using nodejs, not browser 
+
+
+let name ="zaid"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//biginit 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assign
+//symbol => unique
+
+
+// object
+
+
+// to know the type of the value we use:-
+console.log(typeof age)
+console.log(typeof "name")
+console.log(typeof isLoggedIn)
+console.log(typeof null)//object
+console.log(typeof undefined)
+console.log(typeof Symbol('id'))

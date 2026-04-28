30d7d33d0765b64f09226647eb5b2483cb00a659
zaidullah022 <zaidullah022@gmail.com>
2026-04-28 08:04:48 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+
+console.log(typeof (score));
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+
+
+//"33" => 33
+//""33abc" => NaN
+//"abc33" => NaN
+
+let isLoggedIn = "true"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);      
+
+//"" => false
+//" " => true
+//"false" => true
+//"true" => true
+//0 => false
+//1 => true

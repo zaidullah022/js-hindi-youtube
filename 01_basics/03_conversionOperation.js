ce35120408e3cd138d16bd4eaafd7dd3004a71d2
zaidullah022 <zaidullah022@gmail.com>
2026-04-28 10:37:33 +0000

    04_comparisio

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = "33"
 
 
-console.log(typeof (score));
-console.log(typeof score);
+//console.log(typeof (score));
+//console.log(typeof score);
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 
 //"33" => 33
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "true"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);      
+//console.log(booleanIsLoggedIn);      
 
 //"" => false
 //" " => true
@@ -26,3 +26,37 @@ console.log(booleanIsLoggedIn);
 //"true" => true
 //0 => false
 //1 => true
+
+
+//*****************************************************OPERATIONS******************************************************** */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2); //modulus operator gives the remainder
+
+let str1 = "Hello"
+let str2 = " World"
+//console.log(str1 + str2); //concatenation of strings
+
+//console.log("1" + 2); //string concatenation, "12"
+//console.log(1 + "2"); //string concatenation, "12"
+//console.log("1" + "2"); //string concatenation, "12"
+//console.log(1 + 2); //number addition, 3
+//console.log("1"+2+2)
+// "1"+2+2 => "1"+2 => "12"+2 => "122"          
+//console.log(1+2+"3") //this is according to the guideline of left to right evaluation       
+
+let num1 ,num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+let num1 = 6
+let num2 = "6"
+console.log(num1 == num2);  

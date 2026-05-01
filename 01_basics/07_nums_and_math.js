95aee13d69844f89b3d913c11fb23b471ebec3b7
zaidullah022 <zaidullah022@gmail.com>
2026-05-01 12:17:34 +0000

    07_nums_and_math.js

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,10 @@
+const score = 500
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length); //toString() converts the number to string and then we can get the length of the string
+
+console.log(balance.toFixed(2)); //toFixed() method formats a number using fixed-point notation and returns a string representation of the number with a specified number of digits after the decimal point.
+
+console.log(balance.toString().length);
